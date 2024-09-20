fbd699d1b27e777a1725ddc0188d309689d5871c
zoubinnaba <emmanuelzoubinnaba@gmail.com>
2024-09-20 12:01:41 +0000

    Modifie host setting

--- core/settings.py
+++ core/settings.py
@@ -18,7 +18,7 @@ LOGIN_URL = 'login'
 LOGOUT_REDIRECT_URL = 'login'
 AUTH_USER_MODEL = 'userprofile.UserProfile'
 
-ALLOWED_HOSTS = ['invoices-production-f737.up.railway.app']
+ALLOWED_HOSTS = ['.railway.app']
 
 if DEBUG:
     ALLOWED_HOSTS += ['127.0.0.1', 'localhost']
